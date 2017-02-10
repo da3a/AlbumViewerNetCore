70f7acee315825800360ca12248a12fc47088203
dawa <dawa@HZLL0DAWA1>
2017-02-10 15:56:08 +0000

    Add project files.

---
+++ src/AlbumViewerBusiness/Models/AlbumViewerContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace AlbumViewerBusiness
+{
+    public class AlbumViewerContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+
+        public AlbumViewerContext(DbContextOptions<AlbumViewerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Album>().ToTable("Albums");
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+
+        //    if (optionsBuilder.IsConfigured)
+        //        return;
+
+        //    // Auto configuration
+        //    ConnectionString = Configuration.GetValue<string>("Data:AlbumViewer:ConnectionString");
+        //    optionsBuilder.UseSqlServer(ConnectionString);
+        //}
+
+    }
+}
---
+++ src/AlbumViewerNetCore/Controllers/AlbumViewerApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AlbumViewerBusiness;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlbumViewerNetCore.Controllers
+{
+
+    public class AlbumViewerApiController : Controller
+    {
+        private AlbumViewerContext Context;
+
+        public AlbumViewerApiController(AlbumViewerContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("api/helloworld")]
+        public object Get(string name)
+        {
+            return new
+            {
+                message = $"Hello world, {name}.",
+                time = DateTime.Now
+            };
+        }
+
+        [HttpGet("api/artists")]
+        public async Task<List<Artist>> GetArtists()
+        {
+            return await Context.Artists
+                .OrderBy(a => a.ArtistName)
+                .ToListAsync();
+        }
+    }
+
+}
